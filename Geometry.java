493cc9d89106e03a46460cc30bc5118c4d7fdc24
Chris C <ccamer3940@greenvilleschools.us>
2017-08-31 10:30:44 -0400

    Javadoccomments in progress

--- Geometry.java
+++ Geometry.java
@@ -8,6 +8,11 @@
 public class Geometry 
 {
     
+     /**
+     * Sings the second line
+     *
+     * @param animal (as a word) for which animal is on the farm
+     */
     public static double triangleArea(double base, double height) 
     {
         double area = base * height / 2.0;
@@ -15,22 +20,37 @@ public class Geometry
         return area;
     }
     
+    /**
+     * Sings the second line
+     *
+     * @param animal (as a word) for which animal is on the farm
+     */
     public static double rectangleArea(double length, double width) 
     {
         return length * width;
     }
     
+     /**
+     * Sings the second line
+     *
+     * @param animal (as a word) for which animal is on the farm
+     */
     public static double sphereVolume(double radius) 
     {
         return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
     }
     
-      public static double pararlellogramArea(double base, double height) 
+         /**
+     * Sings the second line
+     *
+     * @param animal (as a word) for which animal is on the farm
+     */
+    public static double paralellogramArea(double base, double height) 
     {
         return base * height;
     }
     
-          public static double trapezoidArea(double base1, double base2, double height) 
+    public static double trapezoidArea(double base1, double base2, double height) 
     {
         return (base1+base2)/2.0 * height;
     }
@@ -57,19 +77,24 @@ public class Geometry
     
           public static double rightTriangleHypotenuse(double height, double length) 
     {
-        Math.pow((Math.pow(height, 2) * Math.pow(length, 2)), 0.5);
+        return Math.pow((Math.pow(height, 2) + Math.pow(length, 2)), 0.5);
     }
     
-          public static double distanceBetweenPoints(double x1, ) 
+          public static double distanceBetweenPoints(double x1, double x2, double y1, double y2) 
     {
-        
+        return Math.pow((Math.pow((x2-x1), 2) * Math.pow((y2-y1), 2)),0.5);
     }
     
-          public static double slopeBetweenPoints() 
+          public static double slopeBetweenPoints(double x1, double x2, double y1, double y2) 
     {
-        
+        return (y1-y2)/(x1-x2);
     }
     
+          public static double triangleAreaSides(double a, double b, double c)
+    {
+           double p = (a+b+c)/2.0;
+           return Math.pow((p*(p-a)*(p-b)*(p-c)), 0.5);
+    }
     
     public static void main(String[] args) 
     {
@@ -81,5 +106,36 @@ public class Geometry
         
         double ra1 = rectangleArea(5.2, 9.3);
         System.out.println(ra1);
+        
+        double pa1 = paralellogramArea(3.0, 4.0);
+        System.out.println(pa1);
+        
+        double tr1 = trapezoidArea(3.0, 4.0, 5.0);
+        System.out.println(tr1);
+        
+        double rp1 = rectangularPrismVolume(3.0, 4.0, 5.0) ;
+        System.out.println(rp1);
+        
+        double cv1 = coneVolume(3.0,4.0);
+        System.out.println(cv1);
+        
+        double rs1 = rectangularPrismSurfaceArea(3.0, 4.0, 5.0);
+        System.out.println(rs1);
+        
+        double ss1 = sphereSurfaceArea(3.0);
+        System.out.println(ss1);
+        
+        double rt1 = rightTriangleHypotenuse(3.0, 4.0);
+        System.out.println(rt1);
+        
+        double dp1 = distanceBetweenPoints(3.0, 4.0, 5.0, 6.0);
+        System.out.println(dp1);
+        
+        double sp1 = slopeBetweenPoints(3.0, 4.0, 5.0, 6.0);
+        System.out.println(sp1);
+        
+        double ts1 = triangleAreaSides(3.0, 4.0, 5.0);
+        System.out.println(ts1);
+        
     }
 }
