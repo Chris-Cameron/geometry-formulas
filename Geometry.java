4d508a5f89b82cc1ea2a0f9f26efd31e8728fd85
Chris C <ccamer3940@greenvilleschools.us>
2017-08-31 23:28:23 -0400

    Added Javadoc Comments

--- Geometry.java
+++ Geometry.java
@@ -9,9 +9,10 @@ public class Geometry
 {
     
      /**
-     * Sings the second line
+     * calculates the area of a triangle
      *
-     * @param animal (as a word) for which animal is on the farm
+     * @param base for the base length of the shape
+     * @param height for the height of the shape
      */
     public static double triangleArea(double base, double height) 
     {
@@ -20,10 +21,11 @@ public class Geometry
         return area;
     }
     
-    /**
-     * Sings the second line
+     /**
+     * calculates the area of a rectangle
      *
-     * @param animal (as a word) for which animal is on the farm
+     * @param length for the length of the shape
+     * @param width for the width of the shape
      */
     public static double rectangleArea(double length, double width) 
     {
@@ -31,71 +33,126 @@ public class Geometry
     }
     
      /**
-     * Sings the second line
+     * calculates the area of a parallelogram
      *
-     * @param animal (as a word) for which animal is on the farm
-     */
-    public static double sphereVolume(double radius) 
-    {
-        return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
-    }
-    
-         /**
-     * Sings the second line
-     *
-     * @param animal (as a word) for which animal is on the farm
+     * @param base for the base length of the shape
+     * @param height for the height of the shape
      */
     public static double paralellogramArea(double base, double height) 
     {
         return base * height;
     }
     
+     /**
+     * calculates the area of a trapezoid
+     *
+     * @param base1 for the smaller base of the shape
+     * @param base2 for the larger base of the shape
+     * @param height for the height of the shape
+     */
     public static double trapezoidArea(double base1, double base2, double height) 
     {
         return (base1+base2)/2.0 * height;
     }
     
-          public static double rectangularPrismVolume(double height, double length, double width) 
+     /**
+     * calculates the volume of a rectangular prism
+     *
+     * @param length for the length of the shape
+     * @param height for the height of the shape
+     * @param width for the width of the shape
+     */
+    public static double rectangularPrismVolume(double height, double length, double width) 
     {
         return height * length * width;
     }
     
-          public static double coneVolume(double radius, double height) 
+     /**
+     * calculates the volume of a cone
+     *
+     * @param radius for the radius of the shape
+     * @param height for the height of the shape
+     */
+    public static double coneVolume(double radius, double height) 
     {
         return Math.PI * Math.pow(radius, 2) * (height / 3.0);
     }
     
-          public static double rectangularPrismSurfaceArea(double length, double width, double height) 
+     /**
+     * calculates the surface area of a rectangular prism
+     *
+     * @param length for the length of the shape
+     * @param height for the height of the shape
+     * @param width for the width of the shape
+     */
+    public static double rectangularPrismSurfaceArea(double length, double width, double height) 
     {
         return 2.0*((length*width)+(length*height)+(height*width));
     }
     
-          public static double sphereSurfaceArea(double radius) 
+     /**
+     * calculates the surface area of a sphere
+     *
+     * @param radius for the radius of the shape
+     */
+    public static double sphereSurfaceArea(double radius) 
     {
         return 4.0 * Math.PI * Math.pow(radius, 2);
     }
     
-          public static double rightTriangleHypotenuse(double height, double length) 
+     /**
+     * calculates the length of a right triangle's hypotenuse
+     *
+     * @param length for the length of the shape (non-hypotenuse side)
+     * @param height for the height of the shape (other non-hypotenuse side)
+     */
+    public static double rightTriangleHypotenuse(double height, double length) 
     {
         return Math.pow((Math.pow(height, 2) + Math.pow(length, 2)), 0.5);
     }
     
-          public static double distanceBetweenPoints(double x1, double x2, double y1, double y2) 
+     /**
+     * calculates the distance between two points based on x,y coordinates
+     *
+     * @param x1 for the x coordinate of the first point
+     * @param y1 for the y coordinate of the first point
+     * @param x2 for the x coordinate of the second point
+     * @param y2 for the y coordinate of the second point
+     */
+    public static double distanceBetweenPoints(double x1, double x2, double y1, double y2) 
     {
         return Math.pow((Math.pow((x2-x1), 2) * Math.pow((y2-y1), 2)),0.5);
     }
     
-          public static double slopeBetweenPoints(double x1, double x2, double y1, double y2) 
+     /**
+     * calculates the slope of a line connecting two points based on x,y coordinates
+     *
+     * @param x1 for the x coordinate of the first point
+     * @param y1 for the y coordinate of the first point
+     * @param x2 for the x coordinate of the second point
+     * @param y2 for the y coordinate of the second point
+     */
+    public static double slopeBetweenPoints(double x1, double x2, double y1, double y2) 
     {
         return (y1-y2)/(x1-x2);
     }
     
-          public static double triangleAreaSides(double a, double b, double c)
+     /**
+     * calculates the area of a triangle based on the length of its sides
+     *
+     * @param a for one side of the triangle
+     * @param b for a side of the triangle that isn't a
+     * @param c for the remaining side of the triangle
+     */
+    public static double triangleAreaSides(double a, double b, double c)
     {
            double p = (a+b+c)/2.0;
            return Math.pow((p*(p-a)*(p-b)*(p-c)), 0.5);
     }
     
+     /**
+     * Prints the results of the geometric formulas
+     */
     public static void main(String[] args) 
     {
         double ta1 = triangleArea(4.2, 8.1);
